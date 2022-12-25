c858d079eb6bddc3e47b2c92a5a6d7ccd07051f6
reaper <saip589@gmail.com>
2022-12-25 15:44:47 +0530

    remove index.html

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -21,6 +21,27 @@ lsp.configure('sumneko_lua', {
   }
 })
 
+local lspconfig = require('lspconfig')
+local configs = require('lspconfig/configs')
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+lspconfig.emmet_ls.setup({
+   -- on_attach = on_attach,
+   capabilities = capabilities,
+   filetypes = { 'html', 'typescriptreact', 'javascriptreact', 'css', 'sass', 'scss', 'less' },
+   init_options = {
+     html = {
+       options = {
+         -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
+         ["bem.enabled"] = true,
+       },
+     },
+   }
+})
+
+
+
 local cmp = require('cmp')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
