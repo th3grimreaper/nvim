6de088ec55588b1ab664c3a5890ae40bb0e0b2e5
reaper <saip589@gmail.com>
2023-01-07 23:41:19 +0530

    configure null_ls format on save

--- after/plugin/null_ls.lua
+++ after/plugin/null_ls.lua
@@ -1,7 +1,6 @@
 local null_ls = require("null-ls")
 
 local formatting = null_ls.builtins.formatting
-local diagnostics = null_ls.builtins.diagnostics
 
 local sources = {
  formatting.prettierd.with({
@@ -13,11 +12,26 @@ local sources = {
  formatting.eslint_d.with({
    filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
  }),
- diagnostics.eslint_d.with({
-    env = {
-    ESLINT_DEFAULT_CONFIG = vim.fn.expand("~/.config/nvim/utils/linter-config/.eslintrc.js"),
-        },
- })
 }
 
-null_ls.setup({ sources = sources })
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+local on_attach = function(client, bufnr)
+  if client.supports_method "textDocument/formatting" then
+    vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = augroup,
+      buffer = bufnr,
+      callback = function()
+        -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+        vim.lsp.buf.format({bufnr = bufnr})
+        end,
+    })
+  end
+end
+
+
+null_ls.setup({
+  debug = true,
+  sources = sources,
+  on_attach = on_attach,
+})
