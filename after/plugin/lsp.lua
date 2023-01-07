6a578a6c1c4422ea673e6cb8b0abbb2dcd545548
reaper <saip589@gmail.com>
2023-01-07 15:01:05 +0530

    fix null-ls formatting

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,71 +1,73 @@
-local lspconfig = require('lspconfig')
-local lsp_defaults =lspconfig.util,
-default_config
+local lsp = require("lsp-zero")
 
-require('mason').setup()
-require('mason-lspconfig').setup({
-  ensure_installed = {
-    'tsserver',
-    'html',
-    'cssls',
-    'eslint'
-  }
+lsp.preset("recommended")
+
+lsp.ensure_installed({
+  'tsserver',
+  'html',
+  'cssls',
+  'sumneko_lua',
 })
 
-require('mason-lspconfig').setup_handlers({
-  function(server)
-    lspconfig[server].setup({})
-  end,
-  ['tsserver'] = function()
-    lspconfig.tsserver.setup({
-      settings = {
-        completions = {
-          completeFunctionCalls = true
+-- Fix Undefined global 'vim'
+lsp.configure('sumneko_lua', {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { 'vim' }
+            }
         }
-      }
-    })
-  end,
-  ['html'] = function()
-    lspconfig.html.setup({
-      settings = {
-        completions = {
-          completeFunctionCalls = true
-        }
-      }
-    })
-  end,
-  ['cssls'] = function()
-    lspconfig.cssls.setup({
-      settings = {
-        completions = {
-          completeFunctionCalls = true
-        }
-      }
-    })
-  end,
-  ['eslint'] = function()
-    lspconfig.eslint.setup({
-      settings = {
-        completions = {
-          completeFunctionCalls = true
-        }
-      }
-    })
-  end,
+    }
 })
 
-local function lsp_keymaps(bufnr)
-	local opts = { noremap = true, silent = true }
-	local keymap = vim.api.nvim_buf_set_keymap
-        keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-        keymap.set("n", "K", vim.lsp.buf.hover, opts)
-        keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
-        keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
-        keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
-        keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
-        keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
-        keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
-        keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
-        keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
-        keymap.set("n", "<leader>lf", ":lua vim.lsp.buf.format()<cr>", opts)
-end
+
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = lsp.defaults.cmp_mappings({
+  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+  ["<C-Space>"] = cmp.mapping.complete(),
+})
+
+lsp.setup_nvim_cmp({
+  mapping = cmp_mappings
+})
+
+lsp.set_preferences({
+    suggest_lsp_servers = false,
+    sign_icons = {
+        error = 'E',
+        warn = 'W',
+        hint = 'H',
+        info = 'I'
+    }
+})
+
+lsp.on_attach(function(client, bufnr)
+  local opts = {buffer = bufnr, remap = false}
+
+  if client.name == "eslint_d" then
+      vim.cmd.LspStop('eslint_d')
+      return
+  end
+
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+  vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+  vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
+  vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
+  vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
+  vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
+  vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
+  vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
+  vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
+  vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+  vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format {async = true} end, opts)
+end)
+
+lsp.setup()
+
+vim.diagnostic.config({
+    virtual_text = true,
+})
+
