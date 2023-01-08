a1273842602bf6154c3eb53cb5db285224a1245e
reaper <saip589@gmail.com>
2023-01-08 19:35:41 +0530

    remove lsp cmp document borders

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -9,10 +9,6 @@ cmp.setup({
       -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
     end,
   },
-  window = {
-    -- completion = cmp.config.window.bordered(),
-    documentation = cmp.config.window.bordered(),
-  },
   mapping = cmp.mapping.preset.insert({
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4), ['<C-Space>'] = cmp.mapping.complete(),
