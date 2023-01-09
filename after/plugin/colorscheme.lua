c48a4a43d77402e2c80c8fce9169bc713349b9f6
reaper <saip589@gmail.com>
2023-01-09 14:51:51 +0530

    enable transparency in gruvbox theme

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -5,5 +5,6 @@ vim.cmd[[
     set background=dark
     let g:gruvbox_material_background = 'soft'
     let g:gruvbox_material_better_performance = 1
+    let g:gruvbox_material_transparent_background = 1
     colorscheme gruvbox-material
 ]]
--- init.lua
+++ init.lua
@@ -1,5 +1,1 @@
-vim.cmd[[
-  au ColorScheme * hi Normal guibg=none ctermbg=none
-  au ColorScheme * hi EndOfBuffer guibg=none ctermbg=none
-]]
 require("reaper")
